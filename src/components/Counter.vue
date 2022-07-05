3df9770019435abea12734137f91baaa607149a4
abwonji95 <abwonji95@gmail.com>
2022-07-05 22:39:02 +0300

    add immages

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+    <img src="../../jason-dent-S53ekmu8KkE-unsplash.jpg" alt="yellow">
+    
+    </div>
+    <div class="counter">
+        {{counter}}
+    </div>
+    <div class="buttons">
+    <Button title="Add task"/>
+    <Button title="Remove  task"/>
+    </div>
+</template>
+<script lang="ts" >
+import Button from './components/Button.vue'
+import {ref} from 'vue'
+
+</script>
+
